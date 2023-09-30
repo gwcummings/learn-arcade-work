014aed45cbc924bb28caf59d0b55600e3b240d52
gwcummings <gwcummings@hotmail.com>
2023-09-30 16:20:34 -0500

    Scratch Work/

---
+++ Scratch Work/print_statements.py
@@ -0,0 +1,6 @@
+print("It was a dark and stormy night,")
+print("suddenly a shot rang out!")
+print("Hi! This is a double quote: \" We use it for printing.")
+#The \" is an escape code here
+
+print("This\nis\nmy\nnext\nexample")
