9cd2e00ae38269b48817e9c551e2fdf48927c9e6
gwcummings <gwcummings@hotmail.com>
2023-09-30 23:53:25 -0500

    Drawing Samples

---
+++ Scratch Work/drawing_samples.py
@@ -0,0 +1,51 @@
+"""
+This is a sample program to show how to draw using the Python programming
+language and Arcade library.
+"""
+
+# Import the "arcade" library
+import arcade
+
+
+# Open a window.
+# From the arcade library, use a function called open_window
+# set the window title to "Drawing Example"
+# set the dimensions (width and height)
+arcade.open_window(600, 600, "Drawing Example")
+
+# Set the background color
+arcade.set_background_color(arcade.csscolor.SKY_BLUE)
+
+# Set the background color with RGB
+#arcade.set_background_color((255, 182, 18))
+# Get ready to draw
+arcade.start_render()
+
+# Draw a rectangle
+# Left of 0, right of 599
+# Top of 300, bottom of 0
+arcade.draw_lrtb_rectangle_filled(0, 599, 300, 0, arcade.csscolor.GREEN)
+
+# Tree trunk
+# Center of 100, 320
+# Width of 20
+# Height of 60
+arcade.draw_rectangle_filled(100, 320, 20, 60, arcade.csscolor.SIENNA)
+
+# Tree Top
+# Center of 100, 350
+# Radius of 30 pixels
+arcade.draw_circle_filled(100, 350, 30, arcade.csscolor.DARK_GREEN)
+
+# Draw an ellipse and react with
+# a center of (300, 300)
+# width of 350
+# height of 200
+arcade.draw_rectangle_filled(200, 320, 20, 60, arcade.csscolor.SIENNA)
+arcade.draw_ellipse_filled(200, 370, 60, 80, arcade.csscolor.DARK_GREEN)
+
+# Finish drawing
+arcade.finish_render()
+
+# Keep the window up until someone closes it
+arcade.run()
